99cce814cb3e48a582bfbbf27c39cfdc89828173
Eugene Lukovnikov <39776088+eugenelukovnikov@users.noreply.github.com>
2025-06-12 18:42:47 +0000

    Поправил ожидание загрузки страницы для klinika-innomed.ru

--- klinika-innomed.ru/test_klinika_innomed.py
+++ klinika-innomed.ru/test_klinika_innomed.py
@@ -2,5 +2,5 @@ import pytest
 
 
 def test_homepage(screenshot_helper, page):
-    page.goto("https://klinika-innomed.ru/")
+    page.goto("https://klinika-innomed.ru/", wait_until="networkidle")
     screenshot_helper.take_and_compare("homepage")
