aa114c2469f09b1f041b3587e834b48d0d2dc3cf
Eugene Lukovnikov <39776088+eugenelukovnikov@users.noreply.github.com>
2025-06-12 19:03:24 +0000

    Поправил ожидание загрузки страницы для klinika-innomed.ru - v4

--- klinika-innomed.ru/test_klinika_innomed.py
+++ klinika-innomed.ru/test_klinika_innomed.py
@@ -2,7 +2,7 @@ import pytest
 
 
 def test_homepage(screenshot_helper, page):
-    page.goto("https://klinika-innomed.ru/", wait_until="networkidle")
+    page.goto("https://klinika-innomed.ru/", wait_until="load")
 
     prev_height = 0
     max_scrolls = 20
@@ -10,7 +10,7 @@ def test_homepage(screenshot_helper, page):
 
     while scrolls < max_scrolls:
         page.evaluate("window.scrollTo(0, document.body.scrollHeight);")
-        page.wait_for_timeout(2000)  # ждем 2 секунды для подгрузки контента
+        page.wait_for_timeout(1000)  # ждем 2 секунды для подгрузки контента
         new_height = page.evaluate("document.body.scrollHeight")
         if new_height == prev_height:
             break  # достигли конца страницы, новых данных нет
